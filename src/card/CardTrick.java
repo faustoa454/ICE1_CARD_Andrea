459367e1219576d579b35c26b9aadc27c9deb4cb
faustoa454 <faustoa@sheridancollege.ca>
2025-05-22 22:49:33 -0400

    student version 1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,11 +4,15 @@
  */
 package card;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author Andrea Fausto 991745454
  */
 public class CardTrick {
     
@@ -19,14 +23,48 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i]=c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Pick a number: ");
+        int number = input.nextInt();
+        input.nextLine();
+        
+        System.out.println("Pick a suit: ");
+        String suit = input.nextLine();
+        
+        Card test1 = new Card();
+        test1.setValue(number);
+        test1.setSuit(suit);
+        
+        
+        boolean matchTest = false;
+        
+        
+        for (Card c2 : magicHand){
+            if(c2.getValue()== number && c2.getSuit().equals(suit)){
+                matchTest = true;
+            }
+        }
+        
+        if (matchTest){
+            System.out.println("They match!!");
+        }
+        else{
+            System.out.println("Oops they don't match");
+        }
+        
+        
+ 
     }
     
 }
